4d978bf88d5d0693272955fabc007e8d5de16dcf
David Buckley <david@davidbuckley.ca>
2023-02-06 20:39:43 -0500

    add hardcoded ping response

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import socket
 
+PONG=b"+PONG\r\n"
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, _ = server_socket.accept()
+    with conn:
+        _ = conn.recv(1024)
+        conn.sendall(PONG)
 
 
 if __name__ == "__main__":
