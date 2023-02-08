9feb9ebbf5c55256e742b8c9686524638789315d
David Buckley <david@davidbuckley.ca>
2023-02-07 22:18:15 -0500

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-import socket
+import asyncio
+
+async def handler(reader, writer):
+    while True:
+        data = await reader.read(1024)
+        if not data:
+            break
+        writer.write(b"+PONG\r\n")
+        await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+
+async def redis_server():
+    server = await asyncio.start_server(handler, host="localhost", port=6379, reuse_port=True)
+    async with server:
+        await server.serve_forever()
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ = server_socket.accept()
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(b"+PONG\r\n")
-
+    asyncio.run(redis_server())
 
 if __name__ == "__main__":
     main()
